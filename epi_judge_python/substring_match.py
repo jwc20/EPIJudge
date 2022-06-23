b8d7b75a44d84ba454033c08c3641a103b1c4ddf
jwc20 <choij2015@gmail.com>
2022-06-23 16:35:48 -0700

    6/23/22

--- epi_judge_python/substring_match.py
+++ epi_judge_python/substring_match.py
@@ -2,8 +2,13 @@ from test_framework import generic_test
 
 
 def rabin_karp(t: str, s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    l = len(s)
+    i = 0
+    while i < len(t):
+        if t[i:i+l] == s:
+            return i
+        i += 1
+    return -1
 
 
 if __name__ == '__main__':
