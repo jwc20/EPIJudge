2619943f2686c4156d15caeb2d8a5afb5257318f
jwc20 <choij2015@gmail.com>
2022-06-23 12:02:21 -0700

    6/23/22

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -4,12 +4,31 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    res = [[1]]
+
+    if n == 0:
+        return []
+
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        # print(res)
+        print(temp)
+        for j in range(len(res[-1]) + 1):
+
+            print(temp[j], temp[j+1])
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+    return res
 
 
-if __name__ == '__main__':
+print(generate_pascal_triangle(5))
+
+"""
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('pascal_triangle.py',
-                                       'pascal_triangle.tsv',
-                                       generate_pascal_triangle))
+        generic_test.generic_test_main(
+            "pascal_triangle.py", "pascal_triangle.tsv", generate_pascal_triangle
+        )
+    )
+    """
